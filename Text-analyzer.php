ae458b984cfb9d3725d2bf396fe20deaa759603e
xulegaspi <xulegaspi@gmail.com>
2015-09-16 14:58:53 +0200

    First commit.

---
+++ Text-analyzer.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Text Analyzer
+Plugin URI: https://github.com/xulegaspi/text-analyzer
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Xurxo Legaspi
+License: GPLv3
+*/
+
