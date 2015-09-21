728b292cc6b2a570dba73919f002ca99a89f5781
xulegaspi <xulegaspi@gmail.com>
2015-09-21 15:44:30 +0200

    Management page added, trying to upload a file.

--- Text-analyzer.php
+++ Text-analyzer.php
@@ -9,6 +9,24 @@ Author: Xurxo Legaspi
 License: GPLv3
 */
 
+/* Set the version number of the plugin. */
+define( 'TextAnalyzer_version', 3.15 );
+
+/* Set the path to the root directory */
+define( 'ABSPATH', dirname(__FILE__) );
+//$path = get_home_path();
+
+/* Set constant path to the plugin directory. */
+define( 'TextAnalyzer_path', plugin_dir_path( __FILE__ ) );
+
+/* Set constant url to the plugin directory. */
+define( 'TextAnalyzer_URL', plugin_dir_url( __FILE__ ) );
+
+/* Set the constant path to the plugin's template directory. */
+define( 'TEMPLATES', TextAnalyzer_path . trailingslashit( 'templates' ), true );
+
+//require_once(TextAnalyzer_path . 'xml_parser.php');
+
 /* Runs when plugin is activated */
 register_activation_hook(__FILE__, 'hello_world_install');
 
@@ -33,7 +51,7 @@ function hello_world_remove() {
 
 add_action('init', 'hello_world');
 function hello_world() {
-    echo "Hello world";
+    echo "Hello world yuuuuujuuuu";
 }
 
 add_action('admin_menu', 'my_plugin_menu');
@@ -41,6 +59,7 @@ add_action('admin_menu', 'my_plugin_menu');
 function my_plugin_menu() {
 
     add_options_page( 'My Plugin Options', 'Text Analyzer', 'manage_options', 'text-analyzer-settings', 'my_plugin_options' );
+	add_management_page('TextAnalyzer', 'TextAnalyzer', 'manage_options', 'text-analyzer', 'text_analyzer_page');
 
 }
 
@@ -48,14 +67,19 @@ function my_plugin_options() {
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
     }
-    echo '<div class="wrap">';
-    echo '<p>Here is where the form would go if I actually had options.</p>';
-//    echo '<form action="/wordpress/wp-admin/options-general.php?page=text-analyzer-settings">';
-    echo '<form action="/wordpress/wp-content/plugins/TextAnalyzer/xml-parser.php">';
-    echo '<input type="file" name="File">';
-    echo '<input type="submit" value="Upload">';
-    echo '</form>';
-    echo '</div>';
+
+    $response = file_get_contents(TEMPLATES . "settings.html");
+    echo $response;
+
+}
+
+function text_analyzer_page() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+
+	$response = file_get_contents(TEMPLATES . "text-analyzer.html");
+	echo $response;
 }
 
 
--- xml-parser.php
+++ xml-parser.php
@@ -6,11 +6,59 @@
  * Time: 2:59
  */
 
-$uploaddir = '/var/www/uploads';
-$file = $_FILES['File'];
+class Upload_Box {
+    public static function get_instance() {
+        if( null == self::$instance ) {
+            self::$instance = new self;
+        }
 
-echo "Hello";
+        return self::$instance;
+    }
 
-//echo '<pre>';
-//print_r($_FILES);
-//echo '</pre>';
+    private function __construct() {
+        add_action( 'add_meta_boxes', array( $this, 'add_file_meta_box'));
+        add_action( 'save_post', array( $this, 'save_custom_meta_data'));
+    }
+
+    public function post_media_display( $post ) {
+
+        wp_nonce_field( plugin_basename(__FILE__), $this->nonce);
+
+    }
+
+    public function save_custom_meta_data( $post_id ) {
+        // First, make sure the user can save the post
+        if( $this->user_can_save( $post_id, $this->nonce ) ) {
+
+            // If the user uploaded an image, let's upload it to the server
+            if (!empty($_FILES) && isset($_FILES['post_media'])) {
+
+                // Upload the goal image to the uploads directory, resize the image, then upload the resized version
+                $goal_image_file = wp_upload_bits($_FILES['post_media']['name'], null, wp_remote_get($_FILES['post_media']['tmp_name']));
+
+                // Set post meta about this image. Need the comment ID and need the path.
+                if (false == $goal_image_file['error']) {
+
+                    // Since we've already added the key for this, we'll just update it with the file.
+                    update_post_meta($post_id, 'umb_file', $goal_image_file['url']);
+
+                } // end if/else
+
+            } // end if
+
+        }
+
+    }
+
+    function user_can_save( $post_id, $nonce ) {
+
+        $is_autosave = wp_is_post_autosave( $post_id );
+        $is_revision = wp_is_post_revision( $post_id );
+        $is_valid_nonce = ( isset( $_POST[ $nonce ] ) && wp_verify_nonce( $_POST[ $nonce ], plugin_basename( __FILE__ ) ) );
+
+        // Return true if the user is able to save; otherwise, false.
+        return ! ( $is_autosave || $is_revision ) && $is_valid_nonce;
+
+    }
+
+}
