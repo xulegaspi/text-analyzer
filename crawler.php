b3654c9dc5a2c846c0df6b48e82539716c6c9edf
xulegaspi <xulegaspi@gmail.com>
2015-10-02 19:57:20 +0200

    Crawling posts.

--- crawler.php
+++ crawler.php
@@ -25,28 +25,85 @@ $result = $mysqli->query($query);
 
 echo "Number of entries: " . $result->num_rows;
 $num_rows = $result->num_rows;
-echo "<br />";
+//echo "<br />";
 
 $ii = 0;
-for($ii=0; $ii < $num_rows; $ii++) {
+    for($ii=0; $ii < $num_rows; $ii++) {
 
-    // Access each row
-    $data = $result->fetch_array();
-    $result->free();
+        // Access each row
+        $data = $result->fetch_array();
+//    $result->free();
 //    print_r($data);
-    echo $data['URL'];
-    echo "<br /><hr />";
+        echo $data['URL'];
+        echo "<br /><hr />";
+
+        // Get the data of the blog
+        $id_url = $data['Id'];
+        $url = $data['URL'];
+        echo "Trying to get ---> " . $url . "<br />";
+        $xml = simplexml_load_file($url . "/feed") or die("Error: Cannot create object");
+
+        // Extract the information
+        foreach($xml->children()->children()->item as $item) {
+            echo "The post is ---> " . $item->link;
+            $post_link = $item->link;
+            echo "<br />";
+
+            echo "Publication Date: " . $item->pubDate;
+            $pubDate = $item->pubDate;
+            $pubDate = new DateTime($pubDate);
+            $pubDate->format('Y-m-d H:i:s');
+            echo "<br />";
+
+            $description = $item->description;
+
+//            echo $item->description->next();
+//            foreach($item->children() as $node) {
+//                echo "NODE -> " . $node;
+//                echo "<br />";
+//            }
+//            print_r("CHILDREN -> " . $item->encoded);
+
+            // get the HTML string out of the feed:
+            $htmlString = $item->children('content', true)->encoded;
+            // create DOMDocument for HTML parsing:
+            $htmlParser = new DOMDocument();
+            // load the HTML:
+            $htmlParser->loadHTML($htmlString);
+            // import it into simplexml:
+            $html = simplexml_import_dom($htmlParser);
+
+//            print_r($html->body->p);
+            echo $html->body->asXML();
+//            $postContent = $html->body->p->asXML();
+            $postContent = $html->body->asXML();
+
+//            echo $item
+            echo "<br />";
+            echo "<br />";
+
+            $inserts = "'" . $id_url . "'" . ", " . "'" . $post_link . "'" . ", " . "'" . $description . "'" . ", " . "'" . $postContent . "'" . ", " . "'" . $pubDate->format('Y-m-d H:i:s') . "'";
+
+            $query2 = "INSERT INTO posts (Id_URL, Post_URL, Description, Post, PubDate) VALUES " . "(" . $inserts . ")";
+            $result2 = $mysqli->query($query2);
+
+//            $post = simplexml_load_file()
+        }
 
 
-    $id_url = $data['Id'];
-    $url = $data['URL'];
-    $xml = simplexml_load_file($url . "/feed") or die("Error: Cannot create object");
+
+
 //    file_put_contents("Tmpfile.xml", fopen($url . "/feed", 'r'));
 
-    $query = "UPDATE urls SET Raw_page='$xml->asXML()' WHERE Id='$id_url'";
-    $result = $mysqli->query($query) or die("Error: Query");
+//    echo "ID = " . $id_url . "<br />";
 
-    echo "Page $ii added<br />";
+//        $query = "UPDATE urls SET Raw_page='" . $xml . "' WHERE Id='" . $id_url . "'";
+    //    $query = "UPDATE urls SET Raw_page='$ii' WHERE Id='$id_url'";
+    //    echo "$query<br />";
+//        $result2 = $mysqli->query($query) or die("Error: Query: " . $mysqli->error);
+
+        echo "Page $ii added<br />";
+        echo "Going to page $url";
 
 //    echo $xml->asXML();
 }
