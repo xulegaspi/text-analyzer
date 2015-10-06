2843e9551257acc80cbbd5b3d826b26a78bc1a9d
xulegaspi <xulegaspi@gmail.com>
2015-10-06 18:12:27 +0200

    All Posts crawled and stored.

--- crawler.php
+++ crawler.php
@@ -17,7 +17,7 @@
  */
 
 $target_dir = "uploads/";
-
+set_time_limit(300);
 $mysqli = new mysqli("localhost", "root", "", "textanalyzer1");
 
 $query = "SELECT * FROM urls";
@@ -41,56 +41,63 @@ $ii = 0;
         $id_url = $data['Id'];
         $url = $data['URL'];
         echo "Trying to get ---> " . $url . "<br />";
-        $xml = simplexml_load_file($url . "/feed") or die("Error: Cannot create object");
+        $xml = simplexml_load_file($url . "/feed");
 
-        // Extract the information
-        foreach($xml->children()->children()->item as $item) {
-            echo "The post is ---> " . $item->link;
-            $post_link = $item->link;
-            echo "<br />";
+        if ($xml) {
 
-            echo "Publication Date: " . $item->pubDate;
-            $pubDate = $item->pubDate;
-            $pubDate = new DateTime($pubDate);
-            $pubDate->format('Y-m-d H:i:s');
-            echo "<br />";
+            // Extract the information
+            foreach ($xml->children()->children()->item as $item) {
+                echo "The post is ---> " . $item->link;
+                $post_link = $item->link;
+                echo "<br />";
 
-            $description = $item->description;
+                echo "Publication Date: " . $item->pubDate;
+                $pubDate = $item->pubDate;
+                $pubDate = new DateTime($pubDate);
+                $pubDate->format('Y-m-d H:i:s');
+                echo "<br />";
 
-//            echo $item->description->next();
-//            foreach($item->children() as $node) {
-//                echo "NODE -> " . $node;
-//                echo "<br />";
-//            }
-//            print_r("CHILDREN -> " . $item->encoded);
+                $description = $item->description;
 
-            // get the HTML string out of the feed:
-            $htmlString = $item->children('content', true)->encoded;
-            // create DOMDocument for HTML parsing:
-            $htmlParser = new DOMDocument();
-            // load the HTML:
-            $htmlParser->loadHTML($htmlString);
-            // import it into simplexml:
-            $html = simplexml_import_dom($htmlParser);
+                // get the HTML string out of the feed:
+                $htmlString = $item->children('content', true)->encoded;
+                // create DOMDocument for HTML parsing:
+                $htmlParser = new DOMDocument();
+                // load the HTML:
+                $htmlParser->loadHTML($htmlString);
+                // import it into simplexml:
+                $html = simplexml_import_dom($htmlParser);
 
-//            print_r($html->body->p);
-            echo $html->body->asXML();
-//            $postContent = $html->body->p->asXML();
-            $postContent = $html->body->asXML();
+                //            print_r($html->body->p);
+                //            echo $html->body->asXML();
+                //            $postContent = $html->body->p->asXML();
+                try {
+                    if ($html) {
+                        $postContent = $html->body->asXML();
+                        echo $id_url . " added correctly. <br />";
+                    } else {
+                        echo $id_url . " -----> " . $post_link . " is not working. <br />";
+                        $postContent = "ERROR";
+                    }
+                } catch (Exception $e) {
+                    print_r($e);
+                }
 
-//            echo $item
-            echo "<br />";
-            echo "<br />";
+                //            echo $item
+                echo "<br />";
+                echo "<br />";
 
-            $inserts = "'" . $id_url . "'" . ", " . "'" . $post_link . "'" . ", " . "'" . $description . "'" . ", " . "'" . $postContent . "'" . ", " . "'" . $pubDate->format('Y-m-d H:i:s') . "'";
+                $inserts = "'" . $id_url . "'" . ", " . "'" . $post_link . "'" . ", " . "'" . $description . "'" . ", " . "'" . $postContent . "'" . ", " . "'" . $pubDate->format('Y-m-d H:i:s') . "'";
 
-            $query2 = "INSERT INTO posts (Id_URL, Post_URL, Description, Post, PubDate) VALUES " . "(" . $inserts . ")";
-            $result2 = $mysqli->query($query2);
+                $query2 = "INSERT INTO posts (Id_URL, Post_URL, Description, Post, PubDate) VALUES " . "(" . $inserts . ")";
+                $result2 = $mysqli->query($query2);
 
-//            $post = simplexml_load_file()
+                //            $post = simplexml_load_file()
+            }
+
+        } else {
+            echo "ERROR LOADING ----> " . $url;
         }
-
-
 
 
 //    file_put_contents("Tmpfile.xml", fopen($url . "/feed", 'r'));
