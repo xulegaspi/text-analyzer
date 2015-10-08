ad51302d383032c51e4bb8164922aa1a9947f6f0
xulegaspi <xulegaspi@gmail.com>
2015-10-08 20:29:54 +0200

    Starting word frequency and keywords.

--- data-mining.php
+++ data-mining.php
@@ -26,26 +26,53 @@ for($ii=0; $ii < $num_rows; $ii++) {
     $post = $data['Post'];
     $post_id = $data['Id'];
     $url_id = $data['Id_URL'];
+    $url = $data['Post_URL'];
 
-    $post_xml = simplexml_load_string($post);
     $dom = new DOMDocument();
     $dom->loadHTML($post);
     foreach($dom->getElementsByTagName('a') as $node) {
 
-        $inserts = "'" . $url_id . "'" . ", " . "'" . $dom->saveHTML($node) . "'";
-        $query2 = "INSERT INTO media (Id_Post, Media_URL) VALUES (" . $inserts . ")";
-        $result2 = $mysqli->query($query2);
-        echo $dom->saveHTML($node), PHP_EOL;
+        $query_check = "SELECT * FROM media WHERE Media_URL='" . $dom->saveHTML($node) . "'";
+        $result_check = $mysqli->query($query_check);
+
+        // Check if it's already in the db.
+        if($result_check->num_rows == 0) {
+
+            // Insert the media
+            $inserts = "'" . $url_id . "'" . ", " . "'" . $dom->saveHTML($node) . "'";
+            $query2 = "INSERT INTO media (Id_Post, Media_URL) VALUES (" . $inserts . ")";
+            $result2 = $mysqli->query($query2);
+
+        } else {
+
+//            echo "Already on the DB. <br />";
+
+        }
+//        echo $dom->saveHTML($node), PHP_EOL;
     }
 
-    $post2 = strip_tags($post);
-    $post2 = utf8_decode(utf8_decode($post2));
+    $clean_post = strip_tags($post);
+    $clean_post = utf8_decode(utf8_decode($clean_post));
 
-//    print_r($post_xml->children());
-//    print_r($post_dom);
-//    echo "<br />";
-//    echo $post2;
-//    echo $data['Post_URL'];
-    echo "<br />";
-    echo "<hr />";
+    // Processing words
+    $word_count = str_word_count($clean_post);
+
+    // Explode the posts
+    $words_array = explode(" ", $clean_post);
+
+    foreach($words_array as $single_word) {
+
+        $query1 = "SELECT * FROM keywords WHERE Term='" . $single_word . "'";
+        $result1 = $mysqli->query($query1);
+//        $keywords = $result1->fetch_assoc();
+
+        // Levenshtein
+
+
+    }
+
+    echo "WORD COUNT: " . $word_count . " in ----> " . $url . "<br />";
+//    echo $clean_post . "<br /><hr />";
+
+
 }
