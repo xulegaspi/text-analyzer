73c682df4bba3006f2c4e132d01e031861e60a85
xulegaspi <xulegaspi@gmail.com>
2015-10-26 23:34:26 +0100

    Comments added.

--- Visualize/js/vis2.js
+++ Visualize/js/vis2.js
@@ -8,58 +8,49 @@ var gravity = 40 * 0.005;
 
 var path = "data/";
 
-var force;
-var svg;
-var svg2;
-var main;
-var node;
-var label;
+var force;       // Force layout for the buuble chart
+var svg_bubble;  // Bubble Chart SVG
+var svg_bars;    // Bar Chart SVG
+var main;        // Bubble Chart
+var node;        // Bubble Chart nodes (bubbles)
+var label;       // Labels of the Bubbles
 
 //d3.json("vis.php", function(error, graph) {
 d3.json(path + "total_freq.json", function(error, graph2) {
     if (error) throw error;
 
-    var filterData = function(freq1, d) {
-        var array = [];
-        var kk = 0;
-        for(var jj = 0; jj < d.length; jj++) {
-            if(d[jj].freq >= freq1) {
-                array[kk] = d[jj];
-                kk++;
-            }
-        }
-        return array;
-    };
-
-    //d3.select('#slider').call(d3.slider());
-
+    /**
+     * Sliders control
+     */
+    // Frequency Slider
     d3.select("#slider1").on("input", function() {
         update(+this.value);
     });
 
+    // Gravity Slider
     d3.select("#slider2").on("input", function() {
         change_gravity(+this.value);
     });
 
-
     var graph = filterData(15, graph2);
-
-    var kind_to_color = function(d) {
-        var resul = d3.scale.linear()
-            .domain([1, 50, 150])
-            .range(["grey", "green"]);
-        return resul(d.freq);
-    };
-
     draw();
-    //change_gravity(gravity);
 
+
+    
+    /**
+     * @name remove
+     * @description erase the whole visualization
+     */
     function remove() {
-        svg.remove();
-        svg2.remove();
+        svg_bubble.remove();
+        svg_bars.remove();
         node.remove();
     }
 
+    /**
+     * @name draw
+     * @description draws the whole visualization
+     */
     function draw() {
 
         force = d3.layout.force()
@@ -73,16 +64,16 @@ d3.json(path + "total_freq.json", function(error, graph2) {
 
         force.start();
 
-        svg = d3.select("#bubble_chart").append("svg")
+        svg_bubble = d3.select("#bubble_chart").append("svg")
             .attr("width", width)
             .attr("height", height);
         //.attr("transform", "translate(600, 0)");
 
-        svg2 = d3.select("#bar_chart").append("svg")
+        svg_bars = d3.select("#bar_chart").append("svg")
             .attr("width", 500)
             .attr("height", graph.length * 20);
 
-        svg2.selectAll("rect")
+        svg_bars.selectAll("rect")
             .data(graph.sort(function(a,b) { return +b.freq - +a.freq; }))
             .enter()
             .append("rect")
@@ -98,7 +89,7 @@ d3.json(path + "total_freq.json", function(error, graph2) {
             })
             .attr("fill", "steelblue");
 
-        svg2.selectAll("text")
+        svg_bars.selectAll("text")
             .data(graph)
             .enter()
             .append("text")
@@ -111,7 +102,7 @@ d3.json(path + "total_freq.json", function(error, graph2) {
                 return d.Term;
             });
 
-        main = svg.append("g")
+        main = svg_bubble.append("g")
             .attr("class", "graph");
 
         node = main.selectAll(".node_circle")
@@ -129,11 +120,8 @@ d3.json(path + "total_freq.json", function(error, graph2) {
         label = main.selectAll(".node_label")
             .data(graph)
             .enter().append("text")
+            .attr("text-anchor", "middle")
             .attr("class", "node_label")
-            .attr("dx", function (d) {
-                return 2 + 0.5 * Math.sqrt(d.freq);
-            })
-            .attr("dy", ".4em")
             .attr("font-family", "Verdana")
             .attr("font-size", 12)
             .style("fill", "#000000")
@@ -148,7 +136,7 @@ d3.json(path + "total_freq.json", function(error, graph2) {
 
             while (++i < n) q.visit(collide(node[i]));
 
-            svg.selectAll("circle")
+            svg_bubble.selectAll("circle")
                 .attr("cx", function (d) {
                     return d.x;
                 })
@@ -156,7 +144,7 @@ d3.json(path + "total_freq.json", function(error, graph2) {
                     return d.y;
                 });
 
-            svg.selectAll("text")
+            svg_bubble.selectAll("text")
                 .attr("dx", function (d) {
                     return d.x;
                 })
@@ -167,6 +155,13 @@ d3.json(path + "total_freq.json", function(error, graph2) {
 
     }
 
+
+    /**
+     * @name collide
+     * @description Manages and avoid the overlay between nodes
+     * @param node
+     * @returns {Function}
+     */
     function collide(node) {
         var r = node.radius + 16,
             nx1 = node.x - r,
@@ -191,6 +186,11 @@ d3.json(path + "total_freq.json", function(error, graph2) {
         };
     }
 
+    /**
+     * @name update
+     * @description Updates the charts with the new data, adding or removing depending on the applied filters
+     * @param slider1
+     */
     function update(slider1) {
 
         d3.select("#slider1-value").text(slider1);
@@ -204,6 +204,11 @@ d3.json(path + "total_freq.json", function(error, graph2) {
 
     }
 
+    /**
+     * @name change_gravity
+     * @description Updates the bubble chart applying the new gravity property
+     * @param slider2
+     */
     function change_gravity(slider2) {
         d3.select("#slider2-value").text(slider2);
         d3.select("#slider_gravity").property("value", slider2);
@@ -212,6 +217,38 @@ d3.json(path + "total_freq.json", function(error, graph2) {
 
         remove();
         draw();
+    }
+
+    /**
+     * @name filterData
+     * @description Filters the raw data returning only the values that are higher than the given frequency
+     * @param freq1
+     * @param d
+     * @returns {Array}
+     */
+    function filterData(freq1, d) {
+        var array = [];
+        var kk = 0;
+        for(var jj = 0; jj < d.length; jj++) {
+            if(d[jj].freq >= freq1) {
+                array[kk] = d[jj];
+                kk++;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * @name kind_to_color
+     * @description Codifies some information in the color
+     * @param d
+     * @returns {*}
+     */
+    function kind_to_color(d) {
+        var resul = d3.scale.linear()
+            .domain([1, 50, 150])
+            .range(["grey", "green"]);
+        return resul(d.freq);
     }
 
 });
